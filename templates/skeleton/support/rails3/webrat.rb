d4c4494a09b8119c119f6a154ae4865b847bc519
Kristian Mandrup <kmandrup@gmail.com>
2010-02-19 20:21:42 -0500

    refactored support structure

--- templates/skeleton/support/rails3/webrat.rb
+++ templates/skeleton/support/rails3/webrat.rb
@@ -4,18 +4,13 @@ require 'webrat/core/matchers'
 # Rails version: '<%= rails_version %>'
 
 Webrat.configure do |config|  
-<% if rails_version == 2 %>  
-  config.mode = :rails
-<% else %>
   config.mode = :rake
-<% end %>
   config.open_error_files = false # Set to true if you want error pages to pop up in the browser
 end
 
-<% unless rails_version == 2 %>                        
 class Cucumber::Rails::World
   include Webrat::Methods
   include Webrat::Matchers
 end                
-<% end %>
+
 
